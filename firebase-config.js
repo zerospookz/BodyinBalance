490c286eff356b685a123c5a7bf62dea18031079
zerospookz <viktor.v.velev@gmail.com>
2026-02-25 08:36:25 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAHf_pVPAt5jq_yjN_y5Cfz8d1DnS3dG0",
+  apiKey: "AIzaSyAIhf_pVPAt5iq_vjN_y5CfZ8d1DnS3dG0",
   authDomain: "vvv-personal-training.firebaseapp.com",
   projectId: "vvv-personal-training",
   storageBucket: "vvv-personal-training.firebasestorage.app",
   messagingSenderId: "359358492040",
   appId: "1:359358492040:web:4c0d8664fe4ebf06b79e3d",
-  measurementId: "G-75Z40FMH83"
+  measurementId: "G-75Z4QFMH83"
 };
